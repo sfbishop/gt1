a47624ac5d3d0816ceae69fc8e0ed2165434f0f2
gregdferrell <gferrell20@gmail.com>
2015-03-06 14:36:12 -0400

    updated bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,20 +5,25 @@ class BootStrap {
     def init = { servletContext ->
         environments {
             development {
-                def ftFarm = new FacilityType()
-                ftFarm.code = "FACT1"
-                ftFarm.description = "Type 1."
-                ftFarm.save(failOnError: true, flush: true)
+                def ft1 = new FacilityType()
+                ft1.code = "FT1"
+                ft1.description = "Type 1."
+                ft1.save(failOnError: true, flush: true)
 
-                def ftPackingPlant = new FacilityType()
-                ftPackingPlant.code = "FACT2"
-                ftPackingPlant.description = "Type 2."
-                ftPackingPlant.save(failOnError: true, flush: true)
+                def ft2 = new FacilityType()
+                ft2.code = "FT2"
+                ft2.description = "Type 2."
+                ft2.save(failOnError: true, flush: true)
 
-                def ftWarehouse = new FacilityType()
-                ftWarehouse.code = "FACT3"
-                ftWarehouse.description = "Type 3."
-                ftWarehouse.save(failOnError: true, flush: true)
+                def ft3 = new FacilityType()
+                ft3.code = "FT3"
+                ft3.description = "Type 3."
+                ft3.save(failOnError: true, flush: true)
+
+                def ft4 = new FacilityType()
+                ft4.code = "FT4"
+                ft4.description = "Type 4."
+                ft4.save(failOnError: true, flush: true)
             }
 
             test {
